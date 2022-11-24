4c8fd4eb0f6d6023689b7472aae86cfea89e1216
Tu Nombre <you@example.com>
2022-11-24 05:47:46 -0500

    Login layouts terminado

--- app/src/main/java/com/miempresa/pf_enrutados/MainActivity.kt
+++ app/src/main/java/com/miempresa/pf_enrutados/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.miempresa.pf_enrutados
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener(){
+            val activit_login = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(activit_login)
+        }
+
+        btnRegistrar.setOnClickListener(){
+            val activit_registrar = Intent(applicationContext,RegisterActivity::class.java)
+            startActivity(activit_registrar)
+        }
     }
 }

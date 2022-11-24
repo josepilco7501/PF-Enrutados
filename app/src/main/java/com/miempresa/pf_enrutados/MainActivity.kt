868543f018fb7fcd1b3fa8889c137eb2153327a6
Jose Pilco <jose.pilco@tecsup.edu.pe>
2022-11-24 00:41:37 -0500

    Primera version de login

---
+++ app/src/main/java/com/miempresa/pf_enrutados/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miempresa.pf_enrutados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
